78bee5a2d47026f438127e13832f1f3ea9a617ac
Yaron Horsky <horsky.yaron@gmail.com>
2021-12-21 19:16:00 +0200

    server runs through all tests

--- src/client.py
+++ src/client.py
@@ -1,23 +1,50 @@
 import socket
+from datetime import time
+
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
 
 from src.utils import ConnectionSystem
 
+ARG_IP = 0
+ARG_PORT = 1
 
-class FolderMonitor:
+
+class EventHandler(FileSystemEventHandler):
+
+    def on_any_event(self, event):
+        print("something happened")
+
+
+class FolderMonitor(FileSystemEventHandler):
     def __init__(self, folder):
         self.folder = folder
+        self.eventHandler = EventHandler()
 
     def start(self):
-        pass
+        print("[CLIENT]: monitoring...")
+        observer = Observer()
+        observer.schedule(self.eventHandler, self.folder, recursive=True)
+        observer.start()
+        try:
+            while observer.is_alive():
+                observer.join(1)
+        except KeyboardInterrupt:
+            observer.stop()
+        finally:
+            observer.stop()
+            observer.join()
 
 
 class TCPClient(ConnectionSystem):
+
     def __init__(self, params, monitor):
         self.params = params
         self.monitor = monitor
         self.connection = None
 
-    def signup(self, serverIp, serverPort):
+    def signup(self):
+        serverIp, serverPort = self.params[ARG_IP], int(self.params[ARG_PORT])
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connection.connect((serverIp, serverPort))
 
@@ -29,8 +56,3 @@ class TCPClient(ConnectionSystem):
             self.connection.close()
         except Exception:
             print("oops")
-
-    
-    
-
-
--- test/FolderSyncEndToEnd.py
+++ test/FolderSyncEndToEnd.py
@@ -1,58 +1,115 @@
 import threading
-import time
+from time import sleep
 import unittest
 
-# * client signs up to server and uploads empty folder. (WALKING SKELETON) and monitor for changes.
-import socket
-
-import src.client
 from src.client import *
 from src.server import TCPServer
-from src.utils import *
+
+DIR_PATH = "/home/yaron/Desktop/watched"
+SERVER_PORT = 8080
 
 
 class FakeTCPServer(TCPServer):
+
+    def __init__(self, port):
+        super().__init__(port)
+        self.stop = False
+
     def start_server(self):
+        print("thread:", threading.currentThread())
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind(('', self.port))
         print("[FAKE SERVER]: started. listening on port", self.port)
         server.listen(5)
-        client_socket, client_address = server.accept()
-        print("[FAKE SERVER]: client connected!")
-        client_socket.close()
-        print("[FAKE SERVER]: closing client socket")
+        server.settimeout(0.5)
+
+        while not self.stop:
+            try:
+                client_socket, client_address = server.accept()
+                print("[FAKE SERVER]: client connected!")
+                client_socket.close()
+                print("[FAKE SERVER]: closing client socket")
+            except socket.timeout:
+                continue
+
         print("[FAKE SERVER]: closing server.")
         server.close()
 
-
     def run(self):
         # Start the server in a new thread
-        daemon = threading.Thread(name='daemon_server',
+        daemon = threading.Thread(name='server thread',
                                   target=self.start_server)
         daemon.setDaemon(True)  # Set as a daemon so it will be killed once the main thread is dead.
         daemon.start()
 
+    def stopServer(self):
+        self.stop = True
 
-class FakeFolderMonitor(FolderMonitor):
+
+class FakeFolderMonitor:
+    def __init__(self, folder):
+        self.folder = folder
+        self.eventHandler = EventHandler()
+
+    def monitorOnDifThread(self):
+        print("thread:", threading.currentThread())
+        print("[CLIENT]: monitoring...")
+        observer = Observer()
+        observer.schedule(self.eventHandler, self.folder, recursive=True)
+        observer.start()
+        try:
+            while observer.is_alive():
+                observer.join(1)
+        except KeyboardInterrupt:
+            observer.stop()
+        finally:
+            observer.stop()
+            observer.join()
+
     def start(self):
-        print("[MONITOR]: monitoring folder")
+        # Start the server in a new thread
+        daemon = threading.Thread(name='monitor thread',
+                                  target=self.monitorOnDifThread)
+        daemon.setDaemon(True)  # Set as a daemon so it will be killed once the main thread is dead.
+        daemon.start()
+
+
+class FakeEventHandler(FileSystemEventHandler):
+    def on_any_event(self, event):
+        print("something happened")
+
 
 
 class MyTestCase(unittest.TestCase):
 
-    def setUp(self):
-        self.serverPort = 8080
+    @classmethod
+    def setUpClass(self):
+        self.server = FakeTCPServer(SERVER_PORT)
+        self.server.run()
+        sleep(0.1)
 
     def test_FolderSyncEndToEnd(self):
-        server = FakeTCPServer(self.serverPort)
-        server.run()
-        time.sleep(1)
-        params = ["127.0.0.1", "12345", "some path", "some interval"]
-        client = TCPClient(params, FakeFolderMonitor("somePath"))
-        client.signup("127.0.0.1", self.serverPort)
-        time.sleep(1)
+        params = ["127.0.0.1", str(SERVER_PORT), "some path", "some interval"]
+        client = TCPClient(params, FakeFolderMonitor(DIR_PATH))
+        client.signup()
+        sleep(0.1)
         client.startMonitoring()
+        sleep(0.1)
         client.shutdown()
+
+    def test_uploadingEmptyFolderToServer(self):
+        params = ["127.0.0.1", str(SERVER_PORT), "some path", "some interval"]
+        client = TCPClient(params, FakeFolderMonitor(DIR_PATH))
+        client.signup()
+        sleep(0.1)
+        client.startMonitoring()
+        sleep(0.1)
+        client.shutdown()
+
+    @classmethod
+    def tearDownClass(self):
+        self.server.stopServer()
+        sleep(1)
 
 
 if __name__ == '__main__':
